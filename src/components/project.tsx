673c80bf07efbd5c30006015c87135a1fdd0ba06
SamWraith <samirnandan2003@gmail.com>
2024-01-22 13:26:40 +0530

    bug fixed

--- src/components/project.tsx
+++ src/components/project.tsx
@@ -25,7 +25,7 @@ const Project = ({ title, description, tags, imageUrl }: ProjectProps) => {
             className="group mb-3 sm:mb-8 last:mb-0"
         >
             <section className=" bg-gray-100 rounded-lg max-w-[42rem] border border-black/5 overflow-hidden sm:pr-8 relative  sm:h-[20rem]  group-even:pl-8 hover:bg-gray-200 transition ">
-                <div className="pt-4 pb-7 px-5 w-[50%] sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col flex-wrap h-full group-even:ml-[18rem] ">
+                <div className="pt-4 pb-7 px-5 w-[50%] sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col flex-wrap h-full sm:group-even:ml-[18rem] max-sm:group-even:ml-[13.5rem] ">
                     <h3 className="text-2xl font-semibold  ">{title}</h3>
                     <p className="mt-2 leading-relaxed text-gray-700 ">
                         {description}
@@ -45,8 +45,8 @@ const Project = ({ title, description, tags, imageUrl }: ProjectProps) => {
                     src={imageUrl}
                     alt={title}
                     quality={90}
-                    className="absolute top-8 -right-[13.5rem] sm:-right-40 w-[28.25rem]  rounded-t-lg shadow-2xl transition group-hover:scale-[1.04] group-hover:-translate-x-3 group-hover:translate-y-3 group-hover:-rotate-3
-                 group-even:group-hover:translate-x-3 group-even:group-hover:translate-y-3 group-even:group-hover:rotate-3 group-even:right-[initial] group-even:-left-40 "
+                    className="absolute top-8 -right-[15rem] sm:-right-40 w-[28.25rem]  rounded-t-lg shadow-2xl transition group-hover:scale-[1.04] group-hover:-translate-x-3 group-hover:translate-y-3 group-hover:-rotate-3
+                 group-even:group-hover:translate-x-3 group-even:group-hover:translate-y-3 group-even:group-hover:rotate-3 group-even:right-[initial] group-even:-left-[15rem] sm:group-even:-left-40 "
                 />
             </section>
         </motion.div>
