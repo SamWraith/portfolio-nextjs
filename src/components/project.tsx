fe12a12e3b39e020b229c6c7ddcd943a4a430e9e
SamWraith <samirnandan2003@gmail.com>
2024-01-22 13:03:35 +0530

    small device project ui bug solved

--- src/components/project.tsx
+++ src/components/project.tsx
@@ -24,8 +24,8 @@ const Project = ({ title, description, tags, imageUrl }: ProjectProps) => {
             }}
             className="group mb-3 sm:mb-8 last:mb-0"
         >
-            <section className=" bg-gray-100 rounded-lg max-w-[42rem] border border-black/5 overflow-hidden sm:pr-8 relative sm:h-[20rem]  group-even:pl-8 hover:bg-gray-200 transition ">
-                <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full group-even:ml-[18rem] ">
+            <section className=" bg-gray-100 rounded-lg max-w-[42rem] border border-black/5 overflow-hidden sm:pr-8 relative  sm:h-[20rem]  group-even:pl-8 hover:bg-gray-200 transition ">
+                <div className="pt-4 pb-7 px-5 w-[50%] sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col flex-wrap h-full group-even:ml-[18rem] ">
                     <h3 className="text-2xl font-semibold  ">{title}</h3>
                     <p className="mt-2 leading-relaxed text-gray-700 ">
                         {description}
@@ -45,7 +45,7 @@ const Project = ({ title, description, tags, imageUrl }: ProjectProps) => {
                     src={imageUrl}
                     alt={title}
                     quality={90}
-                    className="absolute top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl transition group-hover:scale-[1.04] group-hover:-translate-x-3 group-hover:translate-y-3 group-hover:-rotate-3
+                    className="absolute top-8 -right-40 w-[28.25rem]  rounded-t-lg shadow-2xl transition group-hover:scale-[1.04] group-hover:-translate-x-3 group-hover:translate-y-3 group-hover:-rotate-3
                  group-even:group-hover:translate-x-3 group-even:group-hover:translate-y-3 group-even:group-hover:rotate-3 group-even:right-[initial] group-even:-left-40 "
                 />
             </section>
