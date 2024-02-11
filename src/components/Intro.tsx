1ad40b97a3dad06e6960f8f01b9dfa018c1c80ab
SamWraith <samirnandan2003@gmail.com>
2024-02-11 21:01:15 +0530

    dark mode added

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -73,7 +73,7 @@ const Intro = () => {
                 </span>{" "}
                 with <span className="font-bold">1 year</span> of experience. I
                 enjoy building <span className="italic">sites & apps</span>. My
-                focus is <span className="underline">React (Next.js)</span>{" "}
+                focus is <span className="underline">Web Development</span>{" "}
                 <span> &</span> <span className="underline"> Flutter</span>.
             </motion.h1>
 
@@ -97,7 +97,7 @@ const Intro = () => {
                     <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
                 </Link>
                 <a
-                    className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack "
+                    className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 "
                     href="/CV.pdf"
                     download
                 >
@@ -105,7 +105,7 @@ const Intro = () => {
                     <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
                 </a>
                 <a
-                    className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack "
+                    className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60  "
                     href="https://www.linkedin.com/in/samir-nandan-pandey-2552b7222"
                     target="_blank"
                 >
@@ -113,7 +113,7 @@ const Intro = () => {
                 </a>
 
                 <a
-                    className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack "
+                    className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60 "
                     href="https://github.com/SamWraith"
                     target="_blank"
                 >
