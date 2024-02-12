c4e009c08286a06577c643e88d67c96b038911aa
SamWraith <samirnandan2003@gmail.com>
2024-02-12 21:50:47 +0530

    added speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
 import Header from "@/components/header";
 import ActiveSectionContextProvider from "@/context/active-section-context";
@@ -32,6 +33,7 @@ export default function RootLayout({
                     <ActiveSectionContextProvider>
                         <Header />
                         {children}
+                        <SpeedInsights />
                         <Footer />
 
                         <Toaster position="top-right" />
